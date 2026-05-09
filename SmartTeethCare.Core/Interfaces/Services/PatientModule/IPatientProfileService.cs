ed8fae3431a589d37612d0a194cfc420bb287be3
Heba Nagy <hebanagy3333@gmail.com>
2026-05-09 17:58:00 +0300

    Add IPatientProfileService

---
+++ SmartTeethCare.Core/Interfaces/Services/PatientModule/IPatientProfileService.cs
@@ -0,0 +1,16 @@
+﻿using SmartTeethCare.Core.DTOs.PatientModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartTeethCare.Core.Interfaces.Services.PatientModule
+{
+    public interface IPatientProfileService
+    {
+        Task<PatientProfileDto> GetProfileAsync(string userId);
+
+        Task UpdateProfileAsync(string userId, UpdatePatientProfileDto dto);
+    }
+}
