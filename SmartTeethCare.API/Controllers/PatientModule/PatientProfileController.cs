dcaec047e31be46beb1d3b72e18f0fd132824e67
Heba Nagy <hebanagy3333@gmail.com>
2026-05-09 18:06:08 +0300

    Add Patient Profile Service (Get Profile Endpoint , Update Profile Endpoint)

---
+++ SmartTeethCare.API/Controllers/PatientModule/PatientProfileController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartTeethCare.Core.DTOs.PatientModule;
+using SmartTeethCare.Core.Interfaces.Services.PatientModule;
+using System.Security.Claims;
+
+namespace SmartTeethCare.API.Controllers.PatientModule
+{
+    [Authorize(Roles = "Patient")]
+    [ApiController]
+    [Route("api/patient/profile")]
+    public class PatientProfileController : ControllerBase
+    {
+        private readonly IPatientProfileService _patientProfileService;
+
+        public PatientProfileController(
+            IPatientProfileService patientProfileService)
+        {
+            _patientProfileService = patientProfileService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetProfile()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (string.IsNullOrEmpty(userId))
+                return Unauthorized();
+
+            var result =
+                await _patientProfileService.GetProfileAsync(userId);
+
+            return Ok(result);
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateProfile([FromBody] UpdatePatientProfileDto dto)
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (string.IsNullOrEmpty(userId))
+                return Unauthorized();
+
+            await _patientProfileService.UpdateProfileAsync(
+                userId,
+                dto);
+
+            return Ok(new
+            {
+                Message = "Profile updated successfully"
+            });
+        }
+    }
+}
+
+    
