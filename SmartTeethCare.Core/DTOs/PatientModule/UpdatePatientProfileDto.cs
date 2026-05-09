a2de2d7a80eba4a8f2fb25b14d289abbb9e28436
Heba Nagy <hebanagy3333@gmail.com>
2026-05-09 17:54:51 +0300

    Add Patient Profile Dto & Update Patient Profile Dto

---
+++ SmartTeethCare.Core/DTOs/PatientModule/UpdatePatientProfileDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartTeethCare.Core.DTOs.PatientModule
+{
+    public class UpdatePatientProfileDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
